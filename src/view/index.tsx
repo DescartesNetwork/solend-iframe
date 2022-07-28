d8e37a529b38d38c44f71d36ce9f90880cfc7493
tuphan-dn <tuphan@descartes.network>
2022-07-28 18:29:53 +0800

    update bg

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,12 +1,21 @@
+import { useUI } from '@sentre/senhub'
+
 import EmbededView from '@sentre/embeded-view'
 
 import configs from 'configs'
+import { useEffect } from 'react'
 
 const {
   manifest: { appId },
 } = configs
 
 const View = () => {
+  const { setBackground } = useUI()
+
+  useEffect(() => {
+    setBackground({ light: '#0f1118', dark: '#0f1118' })
+  }, [setBackground])
+
   return (
     <EmbededView
       appId={appId}
