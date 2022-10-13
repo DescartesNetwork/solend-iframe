2d659e6a6df20a191c937bae20bff04dcb1753a2
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-10-13 14:18:33 +0700

    Update senhub v4

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,5 +1,4 @@
-import { useEffect } from 'react'
-import { net, useSetBackground } from '@sentre/senhub'
+import { net } from '@sentre/senhub'
 
 import EmbededView from '@sentre/embeded-view'
 
@@ -11,19 +10,13 @@ const {
 } = configs
 
 const View = () => {
-  const setBackground = useSetBackground()
-
-  useEffect(() => {
-    setBackground({ light: '#0f1118', dark: '#0f1118' })
-  }, [setBackground])
-
   if (net !== 'mainnet') return <MainnetOnly />
   return (
     <EmbededView
       appId={appId}
       src={'https://solend.fi/dashboard/'}
       title="Solend is the autonomous interest rate machine for lending on Solana."
-      wallet={window.sentre.wallet}
+      wallet={window.sentre.solana}
     />
   )
 }
